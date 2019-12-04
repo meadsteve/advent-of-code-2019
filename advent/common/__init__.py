9768318dfe1c412848dc1e015e547d4ed8167106
steveb <steveb@spotify.com>
2019-12-04 08:22:21 +0100

    spinkle some typing onto the file loader

--- advent/common/__init__.py
+++ advent/common/__init__.py
@@ -1,4 +1,7 @@
-def lines_from_file(path):
+from typing import Iterable
+
+
+def lines_from_file(path: str) -> Iterable[str]:
     with open(path) as open_file:
         lines = (line.rstrip('\n') for line in open_file)
         yield from lines
